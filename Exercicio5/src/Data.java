f90a5b949e8a62f786b203c202744c8421294964
Diego Cardoso <dpc1@aluno.ifnmg.edu.br>
2019-09-16 14:59:15 -0300

    novo commit

--- Exercicio5/src/Data.java
+++ Exercicio5/src/Data.java
@@ -1,23 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Carol
- */
 public class Data {
-    private int dia;
-    private int mes;
-    private int ano;
+    int dia;
+    int mes;
+    int ano;
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
     
-    public String imprimir()
-     {
-         String data = this.dia +"/" + this.mes + "/" + this.ano;
-         return data;
-     }
-    
+   
     
 }
